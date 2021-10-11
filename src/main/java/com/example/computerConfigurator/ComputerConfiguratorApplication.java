f11b4e1be71897ba70b505ace7ab5d03000249e4
DmitrySar <youshin.d@gmail.com>
2021-10-11 16:47:59 +0300

    init project

---
+++ src/main/java/com/example/computerConfigurator/ComputerConfiguratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.computerConfigurator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerConfiguratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerConfiguratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/computerConfigurator/ComputerConfiguratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.computerConfigurator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerConfiguratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
