aaf653c09f70c3f5be70a46de9214631d03edc99
DmitrySar <youshin.d@gmail.com>
2021-10-12 11:25:38 +0300

    create index page with controller

---
+++ src/main/java/com/example/computerConfigurator/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.example.computerConfigurator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String mainPage(Model model) {
+
+        return "index";
+    }
+}
